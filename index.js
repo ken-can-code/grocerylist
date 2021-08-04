e6367048a8cf75f9893d80cfdf2546456796b7e8
Ken S <gxcadr@gmail.com>
2021-08-04 01:30:38 -0700

    delete feature added but may be buggy

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const inputBox = document.getElementById('inputbox');
 const submitButton = document.getElementById('submit');
 const listSection = document.getElementById('listsection');
+let idAssigner = 0;
 
 function handleSubmit(event) {
+  idAssigner += 1;
   event.preventDefault(event);
   const inputText = inputBox.value;
   const newListItem = document.createElement('p');
-  newListItem.classList.add('listItem'); // <p class='listItem'>*innerHTML*</p>
-  newListItem.innerHTML = inputText;
+  newListItem.classList.add('listItem'); // <p class='listItem'>*innerHTML*<button /></p>
+  newListItem.setAttribute('id', `deleteMe${idAssigner}`);
+  newListItem.innerHTML = `${inputText} <button id='${idAssigner}'>delete</button>`;
+  console.log('newListItem', newListItem);
   listSection.appendChild(newListItem);
+  const deleteButton = document.getElementById(`deleteMe${idAssigner}`);
+  deleteButton.addEventListener('click', handleDelete);
   inputBox.value = '';
-  // Demonstrating branch coding
+  // features branch
+}
+
+function handleDelete(event) {
+  const elementToDelete = document.getElementById(`deleteMe${event.target.id}`);
+  elementToDelete.remove();
 }
 
 submitButton.addEventListener('click', handleSubmit);
