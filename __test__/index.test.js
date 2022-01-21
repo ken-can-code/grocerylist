aebd1bca2909e734fd5877ad6787f942c415a427
ken-can-code <gxcadr@gmail.com>
2022-01-21 13:04:00 -0800

    some experimentation with code organization for tests

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -8,7 +8,7 @@ const groceryList = require('../index');
 let { key } = groceryList;
 
 describe('test clicking buttons behaves as expected', () => {
-  test('should add item to listItems when inputBox is submitted', async () => {
+  async function launchBrowser() {
     const browser = await puppeteer.launch({
       headless: false,
       slowMo: 80,
@@ -16,12 +16,26 @@ describe('test clicking buttons behaves as expected', () => {
     });
     const page = await browser.newPage();
     await page.goto('file:///Users/ken/Dropbox/grocerylist/index.html');
+    return {
+      browser,
+      page,
+    };
+    // {
+    // browser: browser,
+    // page: page,
+    // }
+  }
+  test('should add first list item to listItems with inputBox text as textContent', async () => {
+    const puppeteerItems = await launchBrowser();
+    const { page, browser } = puppeteerItems;
+    // const page = puppeteerItems.page
+    // const browser = puppeteerItems.browser
     await page.click('#inputBox');
     await page.type('#inputBox', 'Apple');
     await page.click('#submitButton');
     const listItemText1 = await page.$eval(`#listItem${key}`, (el) => el.textContent.slice(0, -17));
     expect(listItemText1).toBe('Apple');
-    key += 1;
+    key += 1; // done manually
     await page.click('#inputBox');
     await page.type('#inputBox', 'Banana');
     await page.click('#submitButton');
